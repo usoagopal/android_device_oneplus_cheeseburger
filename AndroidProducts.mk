9461c0c4ed36bf9fbeb1fcfd665c1258ba07352c
usoagopal <94131230+usoagopal@users.noreply.github.com>
2021-11-13 18:31:09 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cheeseburger.mk
+    $(LOCAL_DIR)/nad_cheeseburger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cheeseburger-user \
-    lineage_cheeseburger-userdebug \
-    lineage_cheeseburger-eng
+    nad_cheeseburger-user \
+    nad_cheeseburger-userdebug \
+    nad_cheeseburger-eng
